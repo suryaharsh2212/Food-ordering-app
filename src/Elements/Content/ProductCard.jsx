0b5b16afac73c8566b8efb972b21a527ddcfb079
surya-prakash603 <suryaraj04266@gmail.com>
2024-04-10 13:50:43 +0530

    ..

--- src/Elements/Content/ProductCard.jsx
+++ src/Elements/Content/ProductCard.jsx
@@ -18,22 +18,20 @@ const ProductCard = ({ name, cusine, price, rating, image }) => {
     <div className='relative top-24 md:top-36'>
 
       <div className=' h-fit w-full p-2' >
-        <div className="card card-compact w-fit h-96 bg-aliceblue shadow-xl transition rounded-lg duration-200 transform hover:bg-white hover:scale-95 hover:rounded-2xl">
+        <div className="card card-compact w-full h-96 bg-aliceblue shadow-xl transition rounded-lg duration-200 transform hover:bg-white hover:scale-95 hover:rounded-2xl">
           <figure className="h-1/2 overflow-hidden">
             <img className="h-full w-full" src={image} alt="Shoes" />
           </figure>
           <div className="card-body h-1/2">
-          <h2 className="card-title">{name} </h2>
-            <div className=' flex justify-start'>
+          <h2 className="card-title text-base">{name} <span className='ml-10'><img className='h-5 w-5' src="https://cdn-icons-png.flaticon.com/128/1040/1040230.png" alt="" /></span>< span className='text-sm '>{rating}</span> </h2>
             
             <h2>{cusine}  </h2>
-            <h2 className='ml-24 flex '><span><img className='h-5 w-5' src="https://cdn-icons-png.flaticon.com/128/1040/1040230.png" alt="" /></span><span>{rating}</span></h2>
-            </div>
-            <h2 className=' font-bold text-xl'>{price}</h2>
+          
+            <h2 className=' font-medium text-xl flex'><span><img className='h-4 w-4' src="https://cdn-icons-png.flaticon.com/128/9375/9375840.png" alt="" /></span><span>{price}</span></h2>
            
             {/* <p>If a dog chews shoes whose shoes does he choose?</p> */}
             <div className="card-actions justify-end">
-              <button className="btn btn-outline w-full" onClick={() => document.getElementById('my_modal_3').showModal()}>Explore Menu</button>
+              <button className="btn bg-red-600 text-white w-full hover:bg-red-700" onClick={() => document.getElementById('my_modal_3').showModal()}>Explore Menu</button>
             </div>
           </div>
         </div>
